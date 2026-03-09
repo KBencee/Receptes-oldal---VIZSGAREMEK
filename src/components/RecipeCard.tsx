4821ab5764cb70801838d2838ad91ddd3d09aef9
ErdeiGergo <21.erdei.gergo.janos@mechwart.com>
2026-03-09 09:06:23 +0100

    refactor recipe card design

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -6,7 +6,7 @@ const RecipeCard = (recipe:RecipeType) => {
   const navigate = useNavigate()
 
   const goToForYou = () => {
-    navigate("/foryou", { state: { recipe } })
+    navigate("/foryou/" + recipe.id)
   }
 
   return (
@@ -14,13 +14,15 @@ const RecipeCard = (recipe:RecipeType) => {
         <img className={styles.myImg} src={recipe.kepUrl ? recipe.kepUrl : "backgroud.jpg"} alt={recipe.nev} title={recipe.nev}/>
         <h2>{recipe.nev}</h2>
         <h3>{recipe.feltoltoUsername}</h3>
-        <p>{recipe.likes}❤️</p>
         <p>{recipe.elkeszitesiIdo} perc</p>
         <p>{recipe.nehezsegiSzint}</p>
         <div className={styles.tags}>
             {recipe.cimkek.map((c,idx) => <span key={idx} className={styles.tag}>{c}</span>)}
         </div>
-        <i className={recipe.mentveVan ? "fa-solid fa-bookmark" : "fa-regular fa-bookmark"}></i>
+        <div className={styles.cardFooter}>
+            <span>{recipe.likes}❤️</span>
+            <i className={recipe.mentveVan ? "fa-solid fa-bookmark" : "fa-regular fa-bookmark"}></i>
+        </div>
     </div>
   )
 }
