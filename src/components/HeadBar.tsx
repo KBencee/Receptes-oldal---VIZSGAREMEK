f67023b148a69f9b83431d3136c7a3425c5eadc0
ErdeiGergo <21.erdei.gergo.janos@mechwart.com>
2026-03-13 10:51:07 +0100

    Refactor HeadBar styles into a separate CSS module and update component to use module styles

--- src/components/HeadBar.tsx
+++ src/components/HeadBar.tsx
@@ -1,10 +1,10 @@
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import styles from '../css/Headbar.module.css'
 
 const HeadBar = () => {
   return (
-    <div className='headBar'>
+    <div className={styles.headBar}>
         <h1>Receptek</h1>
         <p>Ha nincs ötlet a főzéshez...</p>
         <button>Keresés...<FontAwesomeIcon icon={faMagnifyingGlass} /></button>
