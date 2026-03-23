198adbe3598ba20e7f3c1e68c920741733a918bd
ErdeiGergo <21.erdei.gergo.janos@mechwart.com>
2026-03-23 09:36:42 +0100

    Conditional navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,17 @@ import NavMiddle from './NavMiddle'
 import styles from '../css/Nav.module.css'
 import Profile from './Profile'
 import { useMobileContext } from '../context/MobileContextProvider'
+import { useContext } from 'react'
+import { AuthUserContext } from '../context/AuthenticatedUserContextProvider'
 
 const Navbar = () => {
+    const authUser = useContext(AuthUserContext)
     const { isMobile } = useMobileContext()
+
   return (
     <div className={styles.navbar} >
         {isMobile ? "" : <div><img src="my-recipes-logo.png" alt="logo" className={styles.logo}/></div>}
-        <NavMiddle/>
+        {authUser && <NavMiddle/>}
         <Profile/>
     </div>
   )
