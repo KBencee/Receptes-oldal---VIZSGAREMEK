2690ceabf670d8fd523a60372cbe9d0f7fea35a7
K.Bence <kissbence225@gmail.com>
2026-03-30 16:21:08 +0200

    localhost WPF

--- AdminFelület/AdminFelület/obj/Debug/net10.0-windows/AdminFelület_40b4jbux_wpftmp.AssemblyInfo.cs
+++ AdminFelület/AdminFelület/obj/Debug/net10.0-windows/AdminFelület_40b4jbux_wpftmp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminFelület")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa575da5b683b52194a59947e2d54a278274b487")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminFelület")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminFelület")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
